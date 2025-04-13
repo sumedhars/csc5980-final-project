c11aede56bf303b09092eea26f08b81feb23e1e2
ebmden <bassoe@msoe.edu>
2025-04-13 18:42:58 -0500

    removed secrets

--- app/src/main/java/edu/msoe/myapplication/data/JiraApiServiceImpl.kt
+++ app/src/main/java/edu/msoe/myapplication/data/JiraApiServiceImpl.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class JiraApiServiceImpl : JiraApiService {
 
-    private val email = "bassoe@msoe.edu" // Replace with your email
-    private val apiToken = "ATATT3xFfGF0l33rpmjeFMLXburt5t7c8AMPn__OMw4NafgABk8U2xsv-lgdYi_gYFaEXc4OeQ66Rx2MCmZXBAY_K2GT-cY1qndK-qS3V7yem_XdQHJ54avptCxwJzx3iyr_uOz5hgY82eVxKwLBPvq4m7zHyKyYGbHCH-0IKIHXtUkgFckJjeM=D51610E0" // Updated API token
+    private val email: String = "bassoe@msoe.edu" // Replace with your email
+    private val apiToken: String = "api-token" // Updated API token
 
     private val encodedCredentials: String = Base64.encodeToString(
         "$email:$apiToken".toByteArray(), Base64.NO_WRAP
@@ -24,7 +24,7 @@ class JiraApiServiceImpl : JiraApiService {
         .build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://pongstars.atlassian.net/rest/agile/1.0/") // Updated base URL
+        .baseUrl("https://pongstars.atlassian.net/rest/agile/1.0/")
         .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
